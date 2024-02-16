cfc476e4463044915f9408fad36e2845c09afdf9
sanjith <vgsanjith166@gmail.com>
2024-02-16 09:26:19 +0530

    index.js update

--- index.js
+++ index.js
@@ -6,7 +6,19 @@ const iconRef=document.querySelector('div.weather_condition p');
 const conditionRef=document.querySelector('div.weather_condition span');
 const tempRef=document.getElementsByClassName('Temperature');
 
+
+// Get location
 btnRef.addEventListener('click', function(event){
-    console.log(timeRef.value);
+    event.preventDefault();
     console.log(inputRef.value);
+    fetchData();
 })
+
+function fetchData(){
+    fetch("https://api.weatherapi.com/v1/current.json?key=ddbdb41c1725410f9bf54933233112&q=${belukurichi}&aqi=no")
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(function(err){
+            console.log(err);
+        })
+}
